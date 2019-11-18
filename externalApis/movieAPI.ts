5d443f3d1b03f670a4eb4e46144ddb584ed23535
cristhian <rdz.cristhian@gmail.com>
2019-11-17 22:01:16 -0500

    create backend endpoints typescript

--- externalApis/movieAPI.ts
+++ externalApis/movieAPI.ts
@@ -1,8 +1,17 @@
+import Axios from "axios"
+
+const axios = Axios
+
 export async function getTestApi(): Promise<Object> {
-    // call api
-    const obj = {
-        api: "prueba",
-        name: "test api"
+
+    const apiGetTest = await axios.get('https://api.themoviedb.org/3/movie/384018?api_key=7888561a6eff93666be6d54db238535e&language=es-CO')
+    const jsonRes = apiGetTest.data
+    const dataMovie = {
+        title: jsonRes.title,
+        score: jsonRes.vote_average,
+        webUrl: jsonRes.homepage,
+        resume: jsonRes.overview,
     }
-    return obj
+
+    return dataMovie
 }
