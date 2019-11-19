c790ee3bf03ccb57c7156882e77ee8916d406369
ccpinzon <rdz.cristhian@gmail.com>
2019-11-19 15:22:28 -0500

    fixed utl using env - dotenv

---
+++ enviroments.ts
@@ -0,0 +1,5 @@
+import * as envconfig from 'dotenv'
+
+envconfig.config()
+
+export const baseUrlTheMovieApi = process.env.URL_BASE_THE_MOVIE_API
--- externalApis/movieAPI.ts
+++ externalApis/movieAPI.ts
@@ -1,11 +1,27 @@
 import Axios from "axios"
-
+import * as envs from "../enviroments"
 const axios = Axios
 
 export async function getTestApi(): Promise<Object> {
+    const baseUrl = envs.baseUrlTheMovieApi
+    console.log(baseUrl)
+    const context = "movie"
+    const movieId = 384018
+    const url = baseUrl + context
+    let jsonRes: any = {}
+    const params = {
+        api_key : process.env.API_KEY,
+        language: "es-CO"
+    }
+    console.log("url -> " + url)
+    try {
+        const apiGetTest = await axios.get(url+'/'+movieId,{params:params})
+        jsonRes = apiGetTest.data
+    }catch (e) {
+        console.error("errorrequest -> " + e);
+    }
 
-    const apiGetTest = await axios.get('https://api.themoviedb.org/3/movie/384018?api_key=7888561a6eff93666be6d54db238535e&language=es-CO')
-    const jsonRes = apiGetTest.data
+
     const dataMovie = {
         title: jsonRes.title,
         score: jsonRes.vote_average,
