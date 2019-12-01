a4d57e86c96d450bacf2265b80da418921715dba
cristhian <rdz.cristhian@gmail.com>
2019-12-01 15:49:59 -0500

    finish movie api - api gateway

--- api-gateway/routers/movieapi.service.ts
+++ api-gateway/routers/movieapi.service.ts
@@ -1,16 +1,16 @@
 import * as express from "express";
-const apiAdapter = require("./api.adapter")
 import * as envs from "../enviroments"
+
+const apiAdapter = require("./api.adapter")
 
 const router = express.Router()
 
-const BASE_URL = envs.baseUrlTheMovieApi
-
-const api = apiAdapter(BASE_URL)
+const api = apiAdapter(envs.baseUrlTheMovieApi)
 
 // services
 
 const getTrendingMovies = "/movies/trending"
+const getMovieById = "/movies"
 
 
 api.interceptors.request.use((request) => {
@@ -23,6 +23,10 @@ api.interceptors.response.use((response) => {
     console.log(`Response OMS DATA -> : ${JSON.stringify(response.data)}`);
     return response;
 });
+
+/**
+ * trending movies
+ */
 
 router.get(getTrendingMovies.concat('/:time'), (req, res) => {
     console.log("get trending movies")
@@ -39,6 +43,27 @@ router.get(getTrendingMovies.concat('/:time'), (req, res) => {
     });
 
 })
+
+
+/**
+ * movie by id and search
+ */
+router.get(getMovieById.concat('/:id'), (req, res) => {
+    console.log("get movie by id")
+
+    api.get(req.path, {
+        // enviar parametros
+        params: req.query,
+    }).then((resp) => {
+        res.send(resp.data);
+    }).catch((error) => {
+        res.status(error.response.status);
+        res.send(error.response.data);
+        res.status(400).send('Something broke!');
+    });
+
+})
+
 
 module.exports = router
 
