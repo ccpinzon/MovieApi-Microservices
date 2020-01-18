2b5bd8a56f288a6c272ff6f7078628fe070a6d2d
ccpinzon <rdz.cristhian@gmail.com>
2020-01-17 21:40:13 -0500

    add routing tv series

--- api-gateway/routers/movieapi.service.ts
+++ api-gateway/routers/movieapi.service.ts
@@ -8,18 +8,20 @@ const api = apiAdapter.apiAdapter(envs.baseUrlTheMovieApi)
 
 // services
 
-const getTrendingMovies = "/movies/trending"
-const getMovieById = "/movies"
+const getTrendingMovies = "/movie/trending"
+const getMovieById = "/movie"
 
+const getTvById = "/tv"
+const getTrendingTv = "/tv/trending"
 
 api.interceptors.request.use((request) => {
-    console.log(`Starting | Request OMS -> ${JSON.stringify(request)}`);
+    console.log(`Starting | Request api -> ${JSON.stringify(request)}`);
     return request;
 });
 
 api.interceptors.response.use((response) => {
-    console.log(`Response OMS CODE -> : ${response.status}`);
-    console.log(`Response OMS DATA -> : ${JSON.stringify(response.data)}`);
+    console.log(`Response api CODE -> : ${response.status}`);
+    console.log(`Response api DATA -> : ${JSON.stringify(response.data)}`);
     return response;
 });
 
@@ -63,5 +65,47 @@ router.get(getMovieById.concat('/:id'), (req, res) => {
 
 })
 
+
+// series tv
+
+/**
+ * trending movies
+ */
+
+router.get(getTrendingTv.concat('/:time'), (req, res) => {
+    console.log("get trending")
+
+    api.get(req.path, {
+        // enviar parametros
+        params: req.query,
+    }).then((resp) => {
+        res.send(resp.data);
+    }).catch((error) => {
+        res.status(error.response.status);
+        res.send(error.response.data);
+        res.status(400).send('Something broke!');
+    });
+
+})
+
+
+/**
+ * movie by id and search
+ */
+router.get(getTvById.concat('/:id'), (req, res) => {
+    console.log("get by id")
+
+    api.get(req.path, {
+        // enviar parametros
+        params: req.query,
+    }).then((resp) => {
+        res.send(resp.data);
+    }).catch((error) => {
+        res.status(error.response.status);
+        res.send(error.response.data);
+        res.status(400).send('Something broke!');
+    });
+
+})
 
 
