ae5ef1d81db04a905deadcf536c0e58da0c28656
ccpinzon <rdz.cristhian@gmail.com>
2020-01-30 22:27:53 -0500

    finish backend for search series tvs

--- api-gateway/routers/movieapi.service.js
+++ api-gateway/routers/movieapi.service.js
@@ -6,15 +6,17 @@ const apiAdapter = require("./api.adapter");
 exports.router = express.Router();
 const api = apiAdapter.apiAdapter(envs.baseUrlTheMovieApi);
 // services
-const getTrendingMovies = "/movies/trending";
-const getMovieById = "/movies";
+const getTrendingMovies = "/movie/trending";
+const getMovieById = "/movie";
+const getTvById = "/tv";
+const getTrendingTv = "/tv/trending";
 api.interceptors.request.use((request) => {
-    console.log(`Starting | Request OMS -> ${JSON.stringify(request)}`);
+    console.log(`Starting | Request api -> ${JSON.stringify(request)}`);
     return request;
 });
 api.interceptors.response.use((response) => {
-    console.log(`Response OMS CODE -> : ${response.status}`);
-    console.log(`Response OMS DATA -> : ${JSON.stringify(response.data)}`);
+    console.log(`Response api CODE -> : ${response.status}`);
+    console.log(`Response api DATA -> : ${JSON.stringify(response.data)}`);
     return response;
 });
 /**
@@ -38,6 +40,39 @@ exports.router.get(getTrendingMovies.concat('/:time'), (req, res) => {
  */
 exports.router.get(getMovieById.concat('/:id'), (req, res) => {
     console.log("get movie by id");
+    api.get(req.path, {
+        // enviar parametros
+        params: req.query,
+    }).then((resp) => {
+        res.send(resp.data);
+    }).catch((error) => {
+        res.status(error.response.status);
+        res.send(error.response.data);
+        res.status(400).send('Something broke!');
+    });
+});
+// series tv
+/**
+ * trending movies
+ */
+exports.router.get(getTrendingTv.concat('/:time'), (req, res) => {
+    console.log("get trending");
+    api.get(req.path, {
+        // enviar parametros
+        params: req.query,
+    }).then((resp) => {
+        res.send(resp.data);
+    }).catch((error) => {
+        res.status(error.response.status);
+        res.send(error.response.data);
+        res.status(400).send('Something broke!');
+    });
+});
+/**
+ * movie by id and search
+ */
+exports.router.get(getTvById.concat('/:id'), (req, res) => {
+    console.log("get by id");
     api.get(req.path, {
         // enviar parametros
         params: req.query,
